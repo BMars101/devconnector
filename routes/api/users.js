0fe8546806b0fe5d5fa0d30eb8c1867744428e1c
Beth Day <69202254+BMars101@users.noreply.github.com>
2021-01-01 11:20:04 -0500

    working on setting up user registration

---
+++ routes/api/users.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../../models/Users');
+
+//@route POST api/users
+//@desc Register user
+// @access public
+router.post('/', [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists - make a query with mongoose
+
+
+
+      //Get user's gravatar
+
+      //Encrypt password
+
+      //Return JSONwebtoken
+      res.send('User route');
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+
+    }
+
+
+    res.send('User route')
+  });
+
+
+module.exports = router;

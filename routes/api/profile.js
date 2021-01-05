5246f761da49be02a58f74c4374b7190cbf6df82
Beth Day <69202254+BMars101@users.noreply.github.com>
2021-01-05 09:18:54 -0500

    imported axios and config to file

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 const auth = require('../../middleware/auth');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const Profile = require('../../models/Profile');
@@ -319,6 +321,26 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     res.status(500).send('Server Error');
 
   }
-})
+});
+//@route   GET api/profile/github/:username
+//@desc    Get user repos from Github
+//@access  Public
+router.get('/github/:username', async (req, res) => {
+  try {
+    const uri = encodeURI(
+      `http://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+    const headers = {
+      'user-agent': 'node.js',
+      Authorization: `token ${config.get('githubToken')}`
+    };
+
+    const gitHubResponse = await axios.get(uri, { headers });
+    return res.json(gitHubResponse.data);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(404).json({ msg: 'No github profile found' });
+  }
+});
 
 module.exports = router;
